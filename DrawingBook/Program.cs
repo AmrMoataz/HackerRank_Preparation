7b2819fc620074bc3eff2e23627cb5b0d6935282
Amr Moataz <AmrMoatazFCIS@gmail.com>
2022-01-12 22:37:56 +0200

    some more problems

---
+++ DrawingBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DrawingBook
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(PageCount(6, 5));
+		}
+
+		private static int PageCount(int n, int p)
+		{
+			var left = p / 2;
+			var right = (n /2) - (p / 2);
+
+			return left < right ? left : right;
+		}
+	}
+}
